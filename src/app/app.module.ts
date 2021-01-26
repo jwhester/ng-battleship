2d49a43e715a20f187c7469470fe00d1cbeaf53c
james.hester@dynavistics.com <james.hester@dynavistics.com>
2021-01-25 19:31:01 -0600

    added board and player

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
